125738960d27c3813379b394dcc39549129a0f93
Vipul Mehta <tovipul21@gmail.com>
2022-11-26 20:01:03 +0530

    Implemented and tried out the required keyword and SetsRequiredMembers attribute features

---
+++ ConsoleAppDotNet7/PersonalModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RequiredProperties
+{
+    internal class PersonalModel
+    {
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        [SetsRequiredMembers]
+        public PersonalModel()
+        { }
+
+        //[SetsRequiredMembers]
+        //public PersonalModel(string firstName, string lastName)
+        //{}
+    }
+}
--- ConsoleAppDotNet7/Program.cs
+++ ConsoleAppDotNet7/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RequiredProperties;
+
+//PersonalModel pm1 = new("Tim", "Jones");
+
+PersonalModel pm2 = new() { FirstName = "Tim"};
+
+// pm1
+//Console.WriteLine("This data is from pm1 object - " + pm1.FirstName);
+
+// pm2
+Console.WriteLine("This data is from pm2 object - " + pm2.FirstName + " " + pm2.LastName);
+
